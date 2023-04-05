0af129973b5a285660aad0a1a6c747796462308c
Rakesh-puli <Rakesh.Puli@tynybay.com>
2023-04-06 00:30:10 +0530

    dev commit

--- Simple calculator/app.js
+++ Simple calculator/app.js
@@ -1,6 +1,29 @@
 let value1 = document.getElementById("Num1")
 let value2 = document.getElementById("Num2")
+let value3 = document.getElementsByClassName("Result")
 
-function Num1(){
+function addition(){
+    console.log("hello" , value3[0].value )
+value3[0].value = parseInt(value1.value)  + parseInt(value2.value) ;
+    
+}
+
+
+function subtraction(){
+    console.log("hello" , value3[0].value )
+value3[0].value = parseInt(value1.value)  - parseInt(value2.value) ;
+    
+}
+
+
+function multiplication(){
+    console.log("hello" , value3[0].value )
+value3[0].value = parseInt(value1.value) * parseInt(value2.value) ;
+    
+}
+
+function divide(){
+    console.log("hello" , value3[0].value )
+value3[0].value = parseInt(value1.value) % parseInt(value2.value) ;
     
 }
